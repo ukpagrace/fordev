5e5cf4f45b6faf9afbb25f63a806616bff161c2b
ukpagrace <147528351+ukpagrace@users.noreply.github.com>
2024-09-18 10:19:31 +0000

    configured tailwind added header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    return (
+        <div className="flex flex-row justify-between p-3">
+            <h1>Fordevs</h1>
+
+            <div className="flex flex-row justify-between lg:w-2/5 items-center">
+                <a>Challenges</a>
+                <a>Solutions</a>
+                <button className=" flex flex-row items-center px-10 py-3 rounded bg-black text-white">
+                    Login with GitHub
+                    <FaGithub className="ml-2" />
+                </button>
+            </div>
+        </div>
+    )
   }
